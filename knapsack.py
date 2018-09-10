e5b1d3a0ee3fb0379073fb1d994fb8fbbe6ed9d4
urbanchef <roman.mushchinskiy@gmail.com>
2018-09-10 22:59:07 +0300

    refactor

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,5 @@
 def knapsack_fractions():
-    num_of_items, racksack_size = [int(i) for i in input().split()]
+    num_of_items, knapsack_size = [int(i) for i in input().split()]
     items = []
     for _ in range(num_of_items):
         items.append(tuple(int(i) for i in input().split()))
@@ -11,11 +11,11 @@ def knapsack_fractions():
     for item in items_sorted:
         if item[0] == 0:
             continue
-        elif item[1] <= racksack_size:
-            racksack_size -= item[1]
+        elif item[1] <= knapsack_size:
+            knapsack_size -= item[1]
             max_price += item[0]
         else:
-            piece_size = racksack_size / item[1]
+            piece_size = knapsack_size / item[1]
             piece_price = item[0] * piece_size
             max_price += piece_price
             break
